eaf21c7cbd9a82b8d8e19a5e1eb632e71939914d
harshd9 <2023.harsh.duggar@ves.ac.in>
2024-09-17 14:31:02 +0530

    jwt and bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
 
@@ -45,5 +47,48 @@ const userSchema = new Schema({
     }
 
 },{timestamps:true})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+    
+})
+
+userSchema.method.isPasswordCorrect=async function (password) {
+
+    return await bcrypt.compare(password,this.password)
+    
+}
+
+
+userSchema.method.generateAccessToken=function(){
+    jwt.sign(
+    {
+        _id:this._id,
+        username:this.username,
+        fullName:this.fullName,
+        email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.method.generateRandomToken=function(){
+    jwt.sign(
+    {
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
 
 export const User = mongoose.model("User",userSchema);
